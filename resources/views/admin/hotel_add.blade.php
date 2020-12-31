744701f0e71252bc75bb8ecebcb3f669d6120f70
SalehKhaled7 <lost1994soul@gmail.com>
2020-12-31 11:08:23 +0300

    rich text editor added for detail fields in hotel add/edit

--- resources/views/admin/hotel_add.blade.php
+++ resources/views/admin/hotel_add.blade.php
@@ -3,6 +3,10 @@
 @section('CSS')
     <link rel="stylesheet" href="{{asset('assets')}}/dashboard/plugins/select2/dist/css/select2.min.css">
 @endsection
+@section('headerJs')
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css">
+@endsection
 @section('content')
     <div class="main-content">
         <div class="container-fluid">
@@ -79,10 +83,17 @@
                             </div>
                             <div class="form-group row">
                                 <label  class="col-sm-3 col-form-label">Details</label>
-                                <div class="col-sm-9">
-                                    <input type="text" class="form-control" name="details" placeholder="details" >
+                                <div  class="col-sm-9">
+                                    <textarea id="summernote" name="details"></textarea>
+                                    <script>
+                                        $(document).ready(function() {
+                                            $('#summernote').summernote();
+                                        });
+                                    </script>
                                 </div>
+
                             </div>
+
                             <div class="form-group row">
                                 <label  class="col-sm-3 col-form-label">Star</label>
                                 <div class="col-sm-9">
@@ -153,5 +164,6 @@
 @endsection
 @section('footerJs')
     <script src="{{asset('assets')}}/dashboard/plugins/select2/dist/js/select2.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
 @endsection
 
